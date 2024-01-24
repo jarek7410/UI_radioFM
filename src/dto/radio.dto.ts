b2f02ec1fd7ab8460d33c2355b90a371a519f8fd
iniarski <filipmw2002@gmail.com>
2024-01-24 17:14:56 +0100

    Add id to radio.dto
    See previous commit c63c91f5f9b4aa8dfceb1fefd22b0ec64439e20b - Add id to radio stations

--- src/dto/radio.dto.ts
+++ src/dto/radio.dto.ts
@@ -4,6 +4,7 @@ export interface radioDto {
     rooms:room[],
 }
 export interface room{
+    id:number, // see commit c63c91f5f9b4aa8dfceb1fefd22b0ec64439e20b - Add id to radio stations
     audioUrls:
         {
             hls:String,
