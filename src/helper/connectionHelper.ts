14fa2bf855f5b6b628b90b7651cdd2f6e42adced
iniarski <filipmw2002@gmail.com>
2024-01-28 18:44:25 +0100

    Add localhost to regex

--- src/helper/connectionHelper.ts
+++ src/helper/connectionHelper.ts
@@ -1,5 +1,5 @@
 export const checkUrl = (url: string): string|undefined => {
-    const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    const expression = /(?:https?:\/\/)?(?:www\.)?(?:localhost|[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?)/gi;
     const regex = new RegExp(expression);
 
     if (url.match(regex)) {
